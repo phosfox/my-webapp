1ae8ab4da5f06bcca91733b1de5c13e46acd7f13
phosfox <mordredur@googlemail.com>
2019-08-05 18:34:13 +0200

    finishes tut

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
-(defproject my-webapp "0.1.0-SNAPSHOT"
+(defproject my-webapp "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-jetty-adapter "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [hiccup "1.0.5"]
+                 [org.clojure/java.jdbc "0.6.0"]
+                 [com.h2database/h2 "1.4.193"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler my-webapp.handler/app}
   :profiles
--- src/my_webapp/handler.clj
+++ src/my_webapp/handler.clj
@@ -1,11 +1,37 @@
 (ns my-webapp.handler
-  (:require [compojure.core :refer :all]
+  (:require [my-webapp.views :as views] ; add this require
+            [ring.adapter.jetty :as jetty]
+            [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
+  (:gen-class))
 
-(defroutes app-routes
-  (GET "/" [] "Hello World")
+(defroutes app-routes ; replace the generated app-routes with this
+  (GET "/"
+    []
+    (views/home-page))
+  (GET "/add-location"
+    []
+    (views/add-location-page))
+  (POST "/add-location"
+    {params :params}
+    (views/add-location-results-page params))
+  (GET "/location/:loc-id"
+    [loc-id]
+    (views/location-page loc-id))
+  (GET "/all-locations"
+    []
+    (views/all-locations-page))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn -main
+  [& [port]]
+  (let [port (Integer. (or port
+                           (System/getenv "PORT")
+                           5000))]
+    (jetty/run-jetty #'app {:port  port
+                            :join? false})))
